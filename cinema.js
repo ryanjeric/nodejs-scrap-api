7a7cb1a7636e7328c13ae2b5ca64523979fb67d4
ryanjericsabado <ryanjericsabado@yahoo.com>
2020-02-27 20:42:49 +0800

    FIX not geeting 2nd li on Cinema

--- cinema.js
+++ cinema.js
@@ -13,13 +13,16 @@ class Cinema {
             const nextDayLink = await page.evaluate(()=>document.querySelector("#maincontent > ul.nav.nav-tabs.tabs-up > li:nth-child(2) > a").href);
             const showingDate = await page.evaluate(()=>document.querySelector("#schedules > div > p > em").textContent.trim());
             const movies = await page.evaluate(()=>
-                Array.from(document.querySelectorAll('#cinemas .cinema'))
-                    .map((data) => ({
-                        title: data.querySelector('[itemprop="workPresented"] [itemprop="url"] [itemprop="name"]').textContent.trim(),
-                        image: data.querySelector('[itemprop="workPresented"] > div:nth-child(1)').style['backgroundImage'].slice(4, -1).replace(/"/g, ""),
-                        mtrcb: data.querySelector('[itemprop="workPresented"] > div:nth-child(4)').textContent.trim(),
-                        price: data.querySelector('li > div:nth-child(1)').textContent.trim()
-                    })
+                Array.from(document.querySelectorAll('#cinemas .cinema ul')) 
+                    .map((data) => 
+                        Array.from(data.childNodes).map((li) => 
+                            ({
+                                title: li.querySelector('[itemprop="workPresented"] [itemprop="url"] [itemprop="name"]').textContent.trim(),
+                                image: li.querySelector('[itemprop="workPresented"] > div:nth-child(1)').style['backgroundImage'].slice(4, -1).replace(/"/g, ""),
+                                mtrcb: li.querySelector('[itemprop="workPresented"] > div:nth-child(4)').textContent.trim(),
+                                price: li.querySelector('li > div:nth-child(1)').textContent.trim()
+                            })
+                        )
                 )
             )
             await browser.close();
